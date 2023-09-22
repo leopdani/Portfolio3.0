783f406c629b4d6d2b077630afc54b24eb8eb1b7
leopdani <leopdani69@gmail.com>
2023-09-21 21:39:23 -0300

    tentei arrumar bugs na disposição dos itens

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
               /> 
             </div>
             <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
-              <AnimatedText text="Desenvolvimento Web e Design: Transformando Ideias em Sucesso Digital." className='!text-5xl !text-left
+              <AnimatedText text="Desenvolvimento Web e Design: Transformando Ideias em Sucesso Digital." className='!text-6xl !text-left
               xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl
               '/>
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs'>
